40e9eb7232e62e750ccbfb70b17be390abaa2877
juicetein <justin.lao14@gmail.com>
2026-02-14 23:07:25 -0800

    Implement maxHeapify method in HW3 class
    Implement maxHeapify

--- src/HW3.java
+++ src/HW3.java
@@ -13,18 +13,35 @@ class WordFreq {
 }
 
 public class HW3 {
+
+    static void maxHeapify(WordFreq[] a, int heapSize, int i) {
+        while (true) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            int largest = i;
+
+            if (left < heapSize && a[left].frequency > a[largest].frequency) {
+                largest = left;
+            }
+            if (right < heapSize && a[right].frequency > a[largest].frequency) {
+                largest = right;
+            }
+
+            if (largest == i) break;
+
+            WordFreq temp = a[i];
+            a[i] = a[largest];
+            a[largest] = temp;
+
+            i = largest;
+        }
+    }
+
     public static void main(String[] args) {
         WordFreq[] arr = {
-            new WordFreq("happy", 400),
-            new WordFreq("satisfied", 100),
-            new WordFreq("neutral", 300),
-            new WordFreq("would buy again", 200),
-            new WordFreq("terrible", 160),
-            new WordFreq("inconvenient", 900),
-            new WordFreq("difficult to use", 100),
-            new WordFreq("easy to use", 140),
-            new WordFreq("would recommend to friends", 800),
-            new WordFreq("visit the store", 700)
+            // ...
         };
     }
 }
+
