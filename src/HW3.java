cfef6d0c48de8a8bfc3225d2297d2e52160459cb
juicetein <justin.lao14@gmail.com>
2026-02-14 23:10:47 -0800

    Implement main method with WordFreq sample data
    Build heap and print heapified array

--- src/HW3.java
+++ src/HW3.java
@@ -51,10 +51,23 @@ static void buildMaxHeap(WordFreq[] a) {
     }
 }
     
-    public static void main(String[] args) {
-        WordFreq[] arr = {
-            // ...
-        };
-    }
+public static void main(String[] args) {
+    WordFreq[] arr = {
+        new WordFreq("happy", 400),
+        new WordFreq("satisfied", 100),
+        new WordFreq("neutral", 300),
+        new WordFreq("would buy again", 200),
+        new WordFreq("terrible", 160),
+        new WordFreq("inconvenient", 900),
+        new WordFreq("difficult to use", 100),
+        new WordFreq("easy to use", 140),
+        new WordFreq("would recommend to friends", 800),
+        new WordFreq("visit the store", 700)
+    };
+
+    buildMaxHeap(arr);
+    printArray(arr);
+}
+
 }
 
